c9b4a4afedf8901cca9b8279b7345f01069ffd21
nipsalvin <amwaniki.am@gmail.com>
2021-08-24 17:07:14 +0300

    Progress

--- OnePlusTwoEqualsTwelve/src/oneplustwoequalstwelve/OnePlusTwoEqualsTwelve.java
+++ OnePlusTwoEqualsTwelve/src/oneplustwoequalstwelve/OnePlusTwoEqualsTwelve.java
@@ -18,9 +18,10 @@ public class OnePlusTwoEqualsTwelve {
         // TODO code application logic here
         int x = 20;
         int y = 15;
+        
         int a = x/y;    //int / int ==> int
         double b = x/y; //int / int ==> double
-        double c = x/(0.1 * y); //int / (double*int) ==> double
+        double c = x/(1.0 * y); //int / (double*int) ==> double
         double d = x / (double)y; //int / double ==> double
         
         System.out.println(a);
