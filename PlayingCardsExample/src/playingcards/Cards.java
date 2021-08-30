9296298155678396215b6d4a5f12035aa0524b92
nipsalvin <amwaniki.am@gmail.com>
2021-08-30 13:32:36 +0300

    commit 10

--- PlayingCardsExample/src/playingcards/Cards.java
+++ PlayingCardsExample/src/playingcards/Cards.java
@@ -19,7 +19,71 @@ public class Cards {
      * @param cardno integer representation of card 0 = 2 of clubs 13 = 2 of diamonds
      */
     public Cards(int cardno) {
+        //card number divided b 13 then you take the remainder e.g 13/2=6 rem 1....value is 1
         this.rank = cardno % 13;
         this.suit = (cardno/13) % 4;
     }
-}
+    /**
+     *Get the rank of the card
+     * return ran of the card as integer
+     */
+    
+    public int rank() {return rank;}
+    
+    /**
+     * Get the suit of the card
+     * return suit of the card
+     */
+    
+    public int suit() {return suit;}
+    
+    /**
+     * compares 2 ranks to see if they are same
+     * @param c the card to compare to to this card
+     * @return true if same suit, false if otherwise
+     */
+    
+    public boolean sameSuit(Cards c){
+        return (this.suit == c.suit);
+    }
+    /**
+     * Compares 2 cards to see if they are the same rank
+     * @param c the card to compare to this card
+     * @return true if same rank, false if otherwise
+     */
+    
+    public boolean sameRank(Cards c){
+        return (this.rank == c.rank);
+    }
+    
+    /**
+     * Compares 2 cards to see if they are the same
+     * @param c the card to compare to this card
+     * @return true if same card, false if otherwise
+     */
+    
+    public boolean sameCard(Cards c){
+        return(this.sameRank(c) && this.sameSuit(c));
+    }
+    
+    /**
+     * Returns the difference between 2 cards
+     * @params c the card to compare to this card
+     * @return an integer difference between this card and c
+     */
+    public int rankDiff(Cards c){
+        return (this.rank - c.rank);
+    }
+    
+    /**
+     * Converts the cards to a string format for printing
+     * @return the card's representation as a string
+     */
+    @Override
+    public String toString(){
+        String ranks ="23456789TJQKA";
+        String suits = "\u2663\u2666\u2665\u2660"; //CDHS
+        return ranks.charAt(rank)+""+suits.charAt(suit);
+    }
+}   
+
--- PlayingCardsExample/src/playingcardsrun/PlayingCardsRun.java
+++ PlayingCardsExample/src/playingcardsrun/PlayingCardsRun.java
@@ -2,14 +2,26 @@ package playingcardsrun;
 
 //import all classes from playingcards package using *
 //import playingcards.Suits;
+import java.util.Random;
 import playingcards.*;
 
 public class PlayingCardsRun {
 
     public static void main(String[] args) {
-        Cards c = new Cards(Ranks.EIGHT, Suits.DIAMONDS);
+       /* Cards c = new Cards(Ranks.EIGHT, Suits.DIAMONDS);
         Cards d = new Cards(Ranks.SIX, Suits.SPADES);
-    
+        System.out.println(c);
+        System.out.println(d);
+        */
+       
+       Random gen = new Random();
+       Cards c1 = new Cards(Ranks.ACE,Suits.HEARTS);
+       Cards c2 = new Cards(gen.nextInt (52));
+       
+       System.out.printf("Cards: %s %s\n", c1.toString(),c2.toString());
+       System.out.printf("Same rank: %s\n",c1.sameRank(c2));
+       System.out.printf("Same suit: %s\n", c1.sameSuit(c2));
+       System.out.printf("Same card: %s\n",c1.sameCard(c2));
     }
     
 }
